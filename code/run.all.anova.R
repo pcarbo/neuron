9f9c19a487cf40be1297585708928880a13c570f
Peter Carbonetto <peter.carbonetto@gmail.com>
2016-06-28 22:05:11 -0700

    I have a first working draft of run.all.anova.R.

--- code/run.all.anova.R
+++ code/run.all.anova.R
@@ -1,16 +1,26 @@
-# TO DO: Explain here what this script does.
-
-# Script parameters.
-phenotype <- "totalactivity"
-cohort    <- "tcf7l2"
-
-# TO DO: Explain what this means.
-gene <- toupper(cohort)
-
-# TO DO: Explain here what these lines of code are doing.
+# Generates the ANOVA results for all selected phenotypes.
 source("defaults.tcf7l2.R")
 source("defaults.cacna1c.R")
-model.info <- list(tcf7l2  = model.info.tcf7l2,
-                   cacna1c = model.info.cacna1c)
-model.info <- model.info[[cohort]]
+
+# Repeat for each cohort.
+for (cohort in c("tcf7l2","cacna1c")) {
+
+  # The label we use for the cohorts is also the same as the name of
+  # the data table column specifying the gene het/wild-type.
+  gene <- toupper(cohort)
+  
+  # Get the analysis settings for the selected cohort.
+  model.info <- list(tcf7l2  = model.info.tcf7l2,
+                     cacna1c = model.info.cacna1c)
+  model.info <- model.info[[cohort]]
+
+  # Repeat for each of the selected phenotypes.
+  for (phenotype in names(model.info)) {
+    cat("COHORT: ",toupper(cohort),", PHENOTYPE: ",toupper(phenotype),"\n",
+        sep="")
+    source("run.anova.analysis.R")
+    cat("\n")
+  }
+}
+
 
