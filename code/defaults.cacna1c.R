402d93c81925cedab1cfc726ef4d0047ace05015
Peter Carbonetto <peter.carbonetto@gmail.com>
2016-06-30 14:22:45 -0700

    Removed outlier function entries in model.info.

--- code/defaults.cacna1c.R
+++ code/defaults.cacna1c.R
@@ -3,15 +3,13 @@
 #
 #   (a) transformation function
 #   (b) covariate names
-#   (c) outlier removal function
-#   (d) specified outliers
+#   (c) specified outliers
 #
 model.info.cacna1c <- list(
 
   pctdurlight = list(
     transformation   = function(x) logit10(project.onto.interval(x,1,99)/100),
     covariates       = NULL,
-    outlier.function = NULL,
     outliers         = c(55615,55531,55039,55488,55761,55592,55252,
                          55804,55874,55009,55827,55087,55080,55604,
                          55624,55541,55056,55058)),
@@ -19,7 +17,6 @@ model.info.cacna1c <- list(
   d1totalactivity = list(
     transformation   = function (x) log10(x),
     covariates       = NULL,
-    outlier.function = NULL,
     outliers         = c(55614,55359,55799,55570,55689,57024,55234,55246,
                          55248,55884,55873,55551,55525)),
 
@@ -27,21 +24,18 @@ model.info.cacna1c <- list(
     transformation   = function (x) logit10(project.onto.interval(x/400,0.001,
                                                                   0.999)),
     covariates       = "sex",
-    outlier.function = NULL,
     outliers         = c(55109,55359,55405,55323,55595,57056,55826,55269,
                          57029,55006,55008,55676,55827,55021,55638)),
 
   d3.d2totalactivity = list(
     transformation   = NULL,
     covariates       = "d2totalactivity",
-    outlier.function = NULL,
     outliers         = c(55208,55209,57037,55103,55113,55577,55602,55704,
                          55781,55805,57044,55569,55856,55862)),
   
   immobdur = list(
     transformation = function (x) log10(pmax(0.01,x)),
     covariates       = "agouti",
-    outlier.function = NULL,
     outliers         = c(57063,55663,57062,57066,57067,57068,55110,55107,
                          55342,55615,55359,55038,55385,55723,55364,55370, 
                          55857,55569,55232,55180,55220,55825,55804,55184,
@@ -50,6 +44,5 @@ model.info.cacna1c <- list(
   startle = list(
     transformation = function (x) log10(x),
     covariates       = c("sex","d50bw","ppibox1","ppibox2","ppibox3"),
-    outlier.function = NULL,
     outliers         = c(55573,55043,55553,55292,55648,55866,55694,55693, 
                          55184,57029)))
--- code/defaults.tcf7l2.R
+++ code/defaults.tcf7l2.R
@@ -3,7 +3,6 @@
 #
 #   (a) transformation function
 #   (b) covariate names
-#   (c) outlier removal function
 #   (d) specified outliers
 #
 model.info.tcf7l2 <- list(
@@ -11,59 +10,50 @@ model.info.tcf7l2 <- list(
   baseglucose = list(
     transformation   = NULL,
     covariates       = c("sex","bw3","group4B"),
-    outlier.function = function (x) x > 60,
     outliers         = c(49978,50933,50986,51063,51093,52403,52428,53135,
                          53161,53268,53471,53476,53513,53517,53577,53631)),
   
   fastglucose = list(
     transformation   = NULL,
     covariates       = c("sex","bw3"),
-    outlier.function = function (x) x > 60,
     outliers         = c(50964,51031,51032,53113,53146,53177,
                          53241,53532,53633,53647)),
   
   bw2 = list(
     transformation   = NULL,
     covariates       = c("sex","littersize"),
-    outlier.function = NULL,
     outliers         = c(50901,53186,53517)),
   
   d2context = list(
     transformation   = function (x) logit10(project.onto.interval(x,1,99)/100),
     covariates       = c("sex","FCtimeofday","agouti","d1tone"),
-    outlier.function = function (x) x > 2,
     outliers         = c(50970,53104,53141,53652)),
   
   d3tone = list(
     transformation   = function (x) logit10(project.onto.interval(x,1,99)/100),
     covariates       = c("sex","FCtimeofday","agouti"),
-    outlier.function = NULL,
     outliers         = c(50973,51021,52409,52415,53110,53426,53486,
                          53509,53558,53613,53697)),
    
   PPI6 = list(
     transformation   = function (x) logit10(project.onto.interval(x,1,99)/100),
     covariates       = "PPIbox3",
-    outlier.function = function (x) x < (-1),
     outliers         = c(49980,51015,51078,52444,53487,53537,53594,
                          53647,53649)),
   
   PPIstartle = list(
     transformation   = function (x) log10(x),
     covariates       = c("sex","bw2","PPIbox1","PPIbox2","PPIbox3","PPIbox4"),
-    outlier.function = NULL,
     outliers         = c(50927,51015,52406,53181,53250,53278,53431,53487)),
   
   centerduration = list(
     transformation   = function(x) logit10(project.onto.interval(x/400,0.001,
                                                                  0.999)),
     covariates       = "sex",
-    outlier.function = function (x) x < (-1.4) | x > 1.5,
     outliers         = c(50985,51019,51021,52446,53196,53217,53420,
                          53429,53512,53570)),
   
   totalactivity = list(
     transformation   = function(x) log10(x),
     covariates       = NULL,
-    outlier.function = function (x) x < (-0.5) | x > (0.5),
     outliers         = c(51003,53114,53163,53185,53202,53522,53653)))
